dd91701c025840a9d504da910d16660222b8f713
thisgleammm <thisgleam@gmail.com>
2025-02-06 00:37:09 +0700

    Methods

--- src/Main.java
+++ src/Main.java
@@ -9,26 +9,53 @@ class Mahasiswa {
 
 // Class dengan constructor
 class Mahasiswi {
+    // Data member
     String nama;
     String NIM;
     String jurusan;
     double IPK;
     int umur;
 
-    //Counstuctor dipanggil saat object pertama kali dibuat
+    //Counstuctor tanpa parameter
     // Mahasiswi(){
     //     System.out.println("Ini adalah constructor");
     // }
 
     //Constructor dengan parameter
-    Mahasiswi(String inputNama, String inputNIM, String inputjurusan, double inputIPK, int inputumur){
-        nama = inputNama;
-        NIM = inputNIM;
-        jurusan = inputjurusan;
-        IPK = inputIPK;
-        umur = inputumur;
+    Mahasiswi(String nama, String NIM, String jurusan, double IPK, int umur){
+        this.nama = nama;
+        this.NIM = NIM;
+        this.jurusan = jurusan;
+        this.IPK = IPK;
+        this.umur = umur;
+    }
+    
+    //Method tanpa return dan tanpa parameter
+    void show(){
+        System.out.println("Nama : " + this.nama);
+        System.out.println("NIM : " + this.NIM);
+        System.out.println("Jurusan : " + this.jurusan);
+        System.out.println("IPK : " + this.IPK);
+        System.out.println("Umur : " + this.umur);
     }
 
+    //Method tanpa return dan dengan parameter
+    void setNama(String nama){
+        this.nama = nama;
+    }
+
+    //method dengan return dan tanpa parameter
+    String getNama(){
+        return this.nama;
+    }
+    String getNIM(){
+        return this.NIM;
+    }
+
+    //method dengan return dan dengan parameter
+    String sayHi(String message){
+        return message + " juga, nama saya adalah " + this.nama;
+    }
 }
 
 public class Main {
@@ -46,24 +73,22 @@ public class Main {
         System.out.println(mahasiswa1.jurusan);
         System.out.println(mahasiswa1.IPK);
         System.out.println(mahasiswa1.umur);
-        
-        Mahasiswa mahasiswa2 = new Mahasiswa();
-        mahasiswa2.nama = "Aan";
-        mahasiswa2.NIM = "987654321";
-        mahasiswa2.jurusan = "Teknik Teknikan";
-        mahasiswa2.IPK = 3.5;
-        mahasiswa2.umur = 32;
-
-        System.out.println(mahasiswa2.nama);
-        System.out.println(mahasiswa2.NIM);
-        System.out.println(mahasiswa2.jurusan);
-        System.out.println(mahasiswa2.IPK);
-        System.out.println(mahasiswa2.umur);
 
         //Constructor
         Mahasiswi mahasiswi1 = new Mahasiswi("juju", "123456789", "Teknik Informatika", 3.5, 20);
         Mahasiswi mahasiswi2 = new Mahasiswi("jeje", "987654321", "Teknik Teknikan", 3.5, 32);
         System.out.println(mahasiswi1.nama);
         System.out.println(mahasiswi2.nama);
+
+        //Method
+        mahasiswi1.show();
+        mahasiswi1.setNama("Cici");
+        mahasiswi1.show();
+
+        System.out.println(mahasiswi1.getNama());
+        System.out.println(mahasiswi1.getNIM());
+
+        String data = mahasiswi1.sayHi("Hai");
+        System.out.println(data);
     }
 }
